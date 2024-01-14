ca0ad6cca8f998bd4381ced568439a2feddd45ab
Mrutyunjay <mrutyunjaydhal07@gmail.com>
2024-01-14 17:00:48 +0530

    Setup app for json and cookies

--- backEnd/src/app.js
+++ backEnd/src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 // Create APP
 const app = express();
+
+// for use json data
+app.use(express.json());
+// for cookies
+app.use(cookieParser);
 
 //export
 module.exports = app;
