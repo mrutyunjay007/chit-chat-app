da7cb36db147defebdaa6a27e28d56dc0de558ef
Mrutyunjay <mrutyunjaydhal07@gmail.com>
2024-07-27 12:42:15 +0530

    Update cors

--- backEnd/src/socket.js
+++ backEnd/src/socket.js
@@ -10,7 +10,8 @@ const server = require("http").createServer(app);
 const io = new Server(server, {
   pingTimeout: 20000,
   cors: {
-    origin: "https://chit-chat-app-gamma.vercel.app",
+    origin: "chit-chat-app-gamma.vercel.app",
+    methods: ["GET", "POST"],
   },
 });
 
