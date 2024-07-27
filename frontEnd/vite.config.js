843d45de8c0f0d9eb51f6c4737d4d50036d7f9ba
Mrutyunjay <mrutyunjaydhal07@gmail.com>
2024-07-27 18:40:59 +0530

    Add proxy again

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    // proxy: {
-    //   "/api": "https://chit-chat-app-x3yx.onrender.com",
-    // },
+    proxy: {
+      "/api": "https://chit-chat-app-x3yx.onrender.com",
+    },
   },
   plugins: [react()],
 });
