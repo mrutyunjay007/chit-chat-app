6b6b0f2e7638c21351b0764418297462ed87fda6
Mrutyunjay <mrutyunjaydhal07@gmail.com>
2024-07-27 22:39:03 +0530

    test

--- frontEnd/src/Config/socket.js
+++ frontEnd/src/Config/socket.js
@@ -1,9 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://chit-chat-app-x3yx.onrender.com");
+const socket = io("http//localhost/5000");
 
 export const userOnlineStatusSocket = io(
-  "https://chit-chat-app-x3yx.onrender.com/userstatus"
+  // "https://chit-chat-app-x3yx.onrender.com/userstatus"
+  "http//localhost/5000"
 );
 
 export default socket;
