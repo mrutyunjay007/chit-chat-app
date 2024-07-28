f0faf5783ef40b7b6696c527f7ef9f1029141626
Mrutyunjay <mrutyunjaydhal07@gmail.com>
2024-07-28 12:29:26 +0530

    start test for frotnend

--- frontEnd/src/Config/socket.js
+++ frontEnd/src/Config/socket.js
@@ -1,9 +1,11 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://chit-chat-app-x3yx.onrender.com");
+// const socket = io("https://chit-chat-app-x3yx.onrender.com");
+const socket = io("https://localhost:5000");
 
 export const userOnlineStatusSocket = io(
-  "https://chit-chat-app-x3yx.onrender.com/userstatus"
+  // "https://chit-chat-app-x3yx.onrender.com/userstatus"
+  "https://localhost:5000"
 );
 
 export default socket;
