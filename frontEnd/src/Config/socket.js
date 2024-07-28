c64586a40c06a1c884ce018a8a01eb8e0fd54cd9
Mrutyunjay <mrutyunjaydhal07@gmail.com>
2024-07-28 12:32:47 +0530

    end testing of frontend

--- frontEnd/src/Config/socket.js
+++ frontEnd/src/Config/socket.js
@@ -1,11 +1,9 @@
 import { io } from "socket.io-client";
 
-// const socket = io("https://chit-chat-app-x3yx.onrender.com");
-const socket = io("https://localhost:5000");
+const socket = io("https://chit-chat-app-x3yx.onrender.com");
 
 export const userOnlineStatusSocket = io(
-  // "https://chit-chat-app-x3yx.onrender.com/userstatus"
-  "https://localhost:5000"
+  "https://chit-chat-app-x3yx.onrender.com/userstatus"
 );
 
 export default socket;
--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -5,8 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      // "/api": "https://chit-chat-app-x3yx.onrender.com",
-      "/api": "https://localhost:5000",
+      "/api": "https://chit-chat-app-x3yx.onrender.com",
     },
   },
   plugins: [react()],
