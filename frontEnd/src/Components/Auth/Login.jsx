e7cb1bb906b3672d044996290f7c3d5a1898e21c
Mrutyunjay <mrutyunjaydhal07@gmail.com>
2024-07-28 18:04:37 +0530

    add full url to login

--- frontEnd/src/Components/Auth/Login.jsx
+++ frontEnd/src/Components/Auth/Login.jsx
@@ -26,7 +26,7 @@ function Login() {
             };
 
             const res = await axios.post(
-              "/api/v1/login",
+              "https://chit-chat-app-x3yx.onrender.com/api/v1/login",
               { userName: values.userName, password: values.password },
               config
             );
