35da97f0a4594a887c4a2b63a7b253d03ff282e6
Mrutyunjay <mrutyunjaydhal07@gmail.com>
2024-07-31 19:24:54 +0530

    update cookies

--- backEnd/src/app.js
+++ backEnd/src/app.js
@@ -14,8 +14,8 @@ app.use(
   cors({
     origin: "https://chit-chat-app-frontend.onrender.com",
     // origin: "https://localhost:5173",
-    // methods: ["GET", "POST"],
-    // allowedHeaders: ["Authorization", "Content-Type"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Authorization", "Content-Type"],
     credentials: true,
   })
 );
--- backEnd/src/socket.js
+++ backEnd/src/socket.js
@@ -12,9 +12,9 @@ const io = new Server(server, {
   cors: {
     origin: "https://chit-chat-app-frontend.onrender.com",
     // origin: "http://localhost:5173",
-    // methods: ["GET", "POST"],
+    methods: ["GET", "POST"],
     credentials: true,
-    // allowedHeaders: ["Authorization", "Content-Type"],
+    allowedHeaders: ["Authorization", "Content-Type"],
   },
 });
 
