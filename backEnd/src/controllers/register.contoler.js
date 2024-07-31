23b8fbe764f0e70403a82c14212489e4f36b543f
Mrutyunjay <mrutyunjaydhal07@gmail.com>
2024-07-31 20:05:36 +0530

    test

--- backEnd/src/controllers/register.contoler.js
+++ backEnd/src/controllers/register.contoler.js
@@ -186,7 +186,6 @@ const loginController = async (req, res) => {
         // const maxAge = 24 * 60 * 60 * 1000;
 
         const options = {
-          httpOnly: true,
           secure: true,
         };
         //send to client by cookies
