cc8c7107b480d4c8d40e41eb70b144f461461ed0
Mrutyunjay <mrutyunjaydhal07@gmail.com>
2024-07-31 21:21:57 +0530

    add new backend

--- frontEnd/src/Config/socket.js
+++ frontEnd/src/Config/socket.js
@@ -4,7 +4,7 @@ const socket = io("https://chit-chat-app-x3yx.onrender.com");
 // const socket = io("http://localhost:5000");
 
 export const userOnlineStatusSocket = io(
-  "https://chit-chat-app-x3yx.onrender.com/userstatus"
+  "chit-chat-app-mmj3.vercel.app"
   // "http://localhost:5000/userstatus"
 );
 
--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -5,7 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://chit-chat-app-x3yx.onrender.com",
+      // "/api": "https://chit-chat-app-x3yx.onrender.com",
+      "/api": "chit-chat-app-mmj3.vercel.app",
       // "/api": "http://localhost:5000",
     },
   },
