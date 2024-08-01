a4ee01c17f3923177f541c724df0ec8dfe2ecf16
Mrutyunjay <mrutyunjaydhal07@gmail.com>
2024-08-01 13:26:40 +0530

    test

--- backEnd/src/app.js
+++ backEnd/src/app.js
@@ -12,8 +12,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://chit-chat-app-frontend.onrender.com",
-    // origin: "https://localhost:5173",
+    // origin: "https://chit-chat-app-frontend.onrender.com",
+    origin: "https://localhost:5173",
     methods: ["GET", "POST"],
     allowedHeaders: ["Authorization", "Content-Type"],
     credentials: true,
